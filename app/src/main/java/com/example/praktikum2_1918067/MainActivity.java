2b19c93a43d8a418f2348bd0f774a3dffa3740b1
KENTANG <dodywidyanto15@gmail.com>
2021-10-15 20:07:53 +0700

    first commit

---
+++ app/src/main/java/com/example/praktikum2_1918067/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.praktikum2_1918067;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
